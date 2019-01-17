fcde6bac43a3905fc05d4819c0baf08dfb6867e4
Alexander <razgulchik@gmail.com>
2019-01-17 19:53:21 +0300

    new

---
+++ Assets/Scripts/Shoter.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoter : MonoBehaviour
+{
+    public void Fire(float damage)
+    {
+        return;
+    }
+}
