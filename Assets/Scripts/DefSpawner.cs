1ebe9833c7bdd8adc55259872cea06a888c12715
Alexander <razgulchik@gmail.com>
2019-01-31 18:59:36 +0300

    new 31-01

--- Assets/Scripts/DefSpawner.cs
+++ Assets/Scripts/DefSpawner.cs
@@ -8,12 +8,18 @@ public class DefSpawner : MonoBehaviour
 
     private void OnMouseDown()
     {
-        Debug.Log("click");
-        SpawnDefender();
+        SpawnDefender(PointPositionOfDefender());
     }
-    
-    private void SpawnDefender()
+
+    private static Vector2 PointPositionOfDefender()
     {
-        GameObject newDefender = Instantiate(defender, transform.position, Quaternion.identity) as GameObject;
+        Vector2 mousePositionGlobal = Input.mousePosition;
+        Vector2 mousePositionInTheGame = Camera.main.ScreenToWorldPoint(mousePositionGlobal);
+        return mousePositionInTheGame;
+    }
+
+    private void SpawnDefender(Vector2 positionOfDefender)
+    {
+        GameObject newDefender = Instantiate(defender, positionOfDefender, Quaternion.identity) as GameObject;
     }
 }
