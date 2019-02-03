a3e7adc48c10e1a7638b90ead5186262452fe9aa
razgulchik <razgulchik@gmail.com>
2019-02-03 22:52:23 +0300

    030219
    with menu

--- Assets/Scripts/DefSpawner.cs
+++ Assets/Scripts/DefSpawner.cs
@@ -4,22 +4,36 @@ using UnityEngine;
 
 public class DefSpawner : MonoBehaviour
 {
-    [SerializeField] GameObject defender;
+    [SerializeField] Defender defender;
 
     private void OnMouseDown()
     {
         SpawnDefender(PointPositionOfDefender());
+        Debug.Log(PointPositionOfDefender());
     }
 
     private static Vector2 PointPositionOfDefender()
     {
         Vector2 mousePositionGlobal = Input.mousePosition;
         Vector2 mousePositionInTheGame = Camera.main.ScreenToWorldPoint(mousePositionGlobal);
-        return mousePositionInTheGame;
+        Vector2 mousePositionOnTheGrid = SnapToGrid(mousePositionInTheGame);
+        return mousePositionOnTheGrid;
+    }
+
+    private static Vector2 SnapToGrid(Vector2 rawMousePositionInTheGame)
+    {
+        float newX = Mathf.RoundToInt(rawMousePositionInTheGame.x);
+        float newY = Mathf.RoundToInt(rawMousePositionInTheGame.y);
+        return new Vector2(newX, newY);
     }
 
     private void SpawnDefender(Vector2 positionOfDefender)
     {
-        GameObject newDefender = Instantiate(defender, positionOfDefender, Quaternion.identity) as GameObject;
+        Defender newDefender = Instantiate(defender, positionOfDefender, Quaternion.identity) as Defender;
+    }
+
+    public void ChooseDefender(Defender defenderPrefab)
+    {
+        defender = defenderPrefab;
     }
 }
---
+++ Assets/Scripts/DefenderButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderButton : MonoBehaviour
+{
+    [SerializeField] Defender defenderPrefab;
+
+    private void OnMouseDown()
+    {
+        
+        FindObjectOfType<DefSpawner>().ChooseDefender(defenderPrefab);
+
+        var buttons = FindObjectsOfType<DefenderButton>();
+        foreach (DefenderButton button in buttons)
+        {
+            button.GetComponent<SpriteRenderer>().color = new Color32(53, 53, 53, 255);
+        }
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+
+}
