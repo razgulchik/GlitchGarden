493ce2e2387fcd5fd1455509f9f92b6f3bbb35b7
razgulchik <razgulchik@gmail.com>
2019-02-28 20:58:06 +0300

    +level timer

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+    [Tooltip("Level time in SECONDS")]
+    [SerializeField] float levelTime = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Slider>().value = Time.timeSinceLevelLoad / levelTime;
+
+        bool timeIsOut = (Time.timeSinceLevelLoad >= levelTime);
+
+        if(timeIsOut)
+        {
+            Debug.Log("Time is out, level finished");
+        }
+    }
+}
